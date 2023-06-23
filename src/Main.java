ee0664ebe1251e736f35bdde3635d20b5ed5b364
ruslan_v <mailruslan1@gmail.com>
2023-06-23 16:37:47 -0400

    my first jenkins

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.net.Socket;
+
 public class Main {
     public static void main(String[] args) {
         for (int i=0; i<5; i++){
             System.out.println("Hello! You are a cat!");
+            System.out.println("Hi! You are a dog!");
         }
 
     }
